578d95e8767915efd112d14f0eddebe716400cf2
Anna Vasileva <vasilieva.a@icloud.com>
2024-11-27 23:10:48 -0500

    fix horizontal scroll

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,49 +10,49 @@ function Footer() {
       </p>
       <nav className="footer__menu">
         <ul className="footer__navigation">
-        <li className="footer__navigation-item">
-          <a
-            href="/"
-            target="_blank"
-            className="footer__navigation-link footer__navigation-link-home"
-          >
-            Home
-          </a>
+          <li className="footer__navigation-item">
+            <a
+              href="/"
+              target="_blank"
+              className="footer__navigation-link footer__navigation-link-home"
+            >
+              Home
+            </a>
           </li>
           <li className="footer__navigation-item">
-          <a
-            href="https://www.tripleten.com"
-            target="_blank"
-            className="footer__navigation-link footer__navigation-link-tripleten"
-          >
-            TripleTen
-          </a>
+            <a
+              href="https://www.tripleten.com"
+              target="_blank"
+              className="footer__navigation-link footer__navigation-link-tripleten"
+            >
+              TripleTen
+            </a>
           </li>
         </ul>
         <ul className="footer__icons">
-        <li className="footer__icon-item">
-          <a
-            href="https://github.com/anna-amst"
-            target="_blank"
-            className="footer__icon-link"
-          >
-            <img
-              src={githubIcon}
-              className="footer__icon footer__icon-github"
-            />{" "}
-          </a>
+          <li className="footer__icon-item">
+            <a
+              href="https://github.com/anna-amst"
+              target="_blank"
+              className="footer__icon-link"
+            >
+              <img
+                src={githubIcon}
+                className="footer__icon footer__icon-github"
+              />
+            </a>
           </li>
           <li className="footer__icon-item">
-          <a
-            href="https://www.facebook.com/login.php/"
-            target="_blank"
-            className="footer__icon-link"
-          >
-            <img
-              src={facebookIcon}
-              className="footer__icon footer__icon-facebook"
-            />
-          </a>
+            <a
+              href="https://www.facebook.com/login.php/"
+              target="_blank"
+              className="footer__icon-link"
+            >
+              <img
+                src={facebookIcon}
+                className="footer__icon footer__icon-facebook"
+              />
+            </a>
           </li>
         </ul>
       </nav>
